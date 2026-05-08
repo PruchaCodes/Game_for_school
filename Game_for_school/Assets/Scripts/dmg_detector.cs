6db3c766f85d1c21c5f6f0ebe59617b6ded6ff4e
PruchaCodes <pruchavenda1@gmail.com>
2026-05-08 18:16:01 +0200

    Dmg dealing works

--- Game_for_school/Assets/Scripts/dmg_detector.cs
+++ Game_for_school/Assets/Scripts/dmg_detector.cs
@@ -11,23 +11,44 @@ public class dmg_detector : MonoBehaviour
     {
         enemy_stats = GetComponent<enemy_stats>();
         enemy_action_manager = GetComponent<enemy_type1_action_manager>();
+    }
 
+    void Update()
+    {
+        if(player_stats == null || action_manager == null)
+        {
+            FindPlayer();
+        }
+    }
+
+    void FindPlayer()
+    {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        player_stats = player.GetComponent<player_stats>();
-        action_manager = player.GetComponent<Action_manager>();
-        
-        
+
+        if(player != null)
+        {
+            player_stats = player.GetComponent<player_stats>();
+            action_manager = player.GetComponent<Action_manager>();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.CompareTag("Player") && enemy_stats.health > 0 && action_manager.isAttacking && player_stats.stamina > 0)
+        if (player_stats == null || action_manager == null)
+            return;
+
+        if (collider.CompareTag("Player") &&
+            enemy_stats.health > 0 &&
+            action_manager.isAttacking &&
+            player_stats.stamina > 0)
         {
             enemy_stats.health -= player_stats.damage;
             player_stats.stamina -= 10;
         }
 
-        if (collider.CompareTag("Player") && player_stats.health > 0 && enemy_action_manager.isAttacking)
+        if (collider.CompareTag("Player") &&
+            player_stats.health > 0 &&
+            enemy_action_manager.isAttacking)
         {
             player_stats.health -= enemy_stats.damage;
         }
