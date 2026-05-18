d2af93e4ddd8d839d9c6870f7570679745235e17
PruchaCodes <pruchavenda1@gmail.com>
2026-05-18 17:02:47 +0200

    Implementing animation
    Not fully working yet. gonna skip for now

--- Game_for_school/Assets/Scripts/PlayerUIController.cs
+++ Game_for_school/Assets/Scripts/PlayerUIController.cs
@@ -1,17 +1,27 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUIController : MonoBehaviour
 {
     private BattleManager battle_manager;
-
+    private Animator player_animator;
     void Update()
     {
         if (battle_manager == null)
         {
             battle_manager = GameObject.FindGameObjectWithTag("BattleManager").GetComponent<BattleManager>();
         }
+
+        player_animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+
+        
+
+
+        
     }
 
+   
     public void AttackButton()
     {
         if (battle_manager != null)
